1673a2b6b41f4a53260b597da3edced6a0cbe4e8
unknown <sakheralkoran@gmail.com>
2023-04-30 12:42:26 +0300

    finish exercise 1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -22,29 +22,33 @@ function Content(props)
 function Total(props)
 {
   return(   
-    <p>Number of exercises {props.sum}</p>
+    <p>Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
   )
 }
 function App() {
-  const course = 'Half Stack application development'
-  const part1=[{
-    name: 'Fundamentals of React',
-    exercises:10
-  },
-  {
-  name: 'Using props to pass data',
-  exercises:7
- },
- {
-  name: 'State of a component',
-  exercises:14
- }]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={part1}/>
-      <Total sum={part1[0].exercises + part1[1].exercises + part1[1].exercises}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
