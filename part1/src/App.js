80028033b083e03156d1a98cb4aeaa664782b1d3
unknown <sakheralkoran@gmail.com>
2023-04-30 12:32:39 +0300

    Finish exercise 1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,7 +6,7 @@ function Header(props)
 function Part(props)
 {
   return(
-    <p>{props.data[0]} {props.data[1]}</p>
+    <p>{props.data.name} {props.data.exercises}</p>
   )
 }
 function Content(props)
@@ -27,18 +27,24 @@ function Total(props)
 }
 function App() {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1={
+    name: 'Fundamentals of React',
+    exercises:10
+  }
+ const part2={
+  name: 'Using props to pass data',
+  exercises:7
+ }
+ const part3={
+  name: 'State of a component',
+  exercises:14
+ }
 
   return (
     <div>
       <Header course={course}/>
-      <Content data1={[part1,exercises1]} data2={[part2,exercises2]} data3={[part3,exercises3]}/>
-      <Total sum={exercises1 + exercises2 + exercises3}/>
+      <Content data1={part1} data2={part2} data3={part3}/>
+      <Total sum={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
