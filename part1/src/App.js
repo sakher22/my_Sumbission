3344304ce90e91c5ea01e8200d90756786fcb5fd
unknown <sakheralkoran@gmail.com>
2023-04-30 12:38:24 +0300

    finish exercise 1.4

--- part1/src/App.js
+++ part1/src/App.js
@@ -13,9 +13,9 @@ function Content(props)
 {
   return(
     <div>
-      <Part data={props.data1}/>
-      <Part data={props.data2}/>
-      <Part data={props.data3}/>
+      <Part data={props.parts[0]}/>
+      <Part data={props.parts[1]}/>
+      <Part data={props.parts[2]}/>
     </div>
   )
 }
@@ -27,24 +27,24 @@ function Total(props)
 }
 function App() {
   const course = 'Half Stack application development'
-  const part1={
+  const part1=[{
     name: 'Fundamentals of React',
     exercises:10
-  }
- const part2={
+  },
+  {
   name: 'Using props to pass data',
   exercises:7
- }
- const part3={
+ },
+ {
   name: 'State of a component',
   exercises:14
- }
+ }]
 
   return (
     <div>
       <Header course={course}/>
-      <Content data1={part1} data2={part2} data3={part3}/>
-      <Total sum={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts={part1}/>
+      <Total sum={part1[0].exercises + part1[1].exercises + part1[1].exercises}/>
     </div>
   )
 }
